ba4521bd4696949547454bdfaba4ce15e4aada90
probablycorey <probablycorey@github.com>
2018-02-26 17:03:31 -0800

    wtf

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    library: "telepath-crdt",
+    libraryTarget: "umd",
+  }
+}
